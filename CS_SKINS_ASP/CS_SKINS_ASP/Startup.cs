5b687ddb43efbe1fe7002257f12e7b1bd78309ec
Marcel Michel <poissonpourri@gmail.com>
2015-10-27 08:13:10 -0400

    ajout du projet

---
+++ CS_SKINS_ASP/CS_SKINS_ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS_SKINS_ASP.Startup))]
+namespace CS_SKINS_ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
